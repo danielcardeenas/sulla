7ffcb73193ff3e7ac211f3206ded6fabb64026a5
Luís Guilherme <luiisflorido@gmail.com>
2019-08-08 21:44:14 -0300

    Add ignore to lock files

--- src/api/whatsapp.ts
+++ src/api/whatsapp.ts
@@ -8,7 +8,12 @@ import { Id } from './model/id';
 declare module WAPI {
   const waitNewMessages: (rmCallback: boolean, callback: Function) => void;
   const sendMessage: (to: string, content: string) => void;
-  const sendImage: (base64: string, to: string, filename: string, caption: string) => void;
+  const sendImage: (
+    base64: string,
+    to: string,
+    filename: string,
+    caption: string
+  ) => void;
   const sendSeen: (to: string) => void;
   const getAllContacts: () => Contact[];
   const getAllChats: () => Chat[];
@@ -20,130 +25,137 @@ declare module WAPI {
 }
 
 export class Whatsapp {
-  constructor(public page: Page) {
-    this.page = page;
-  }
+         constructor(public page: Page) {
+           this.page = page;
+         }
 
-  /**
-   * Listens to messages received
-   * @returns Observable stream of messages
-   */
-  public onMessage(fn: (message: Message) => void) {
-    this.page.exposeFunction(ExposedFn.OnMessage, (message: Message) =>
-      fn(message)
-    );
-  }
+         /**
+          * Listens to messages received
+          * @returns Observable stream of messages
+          */
+         public onMessage(fn: (message: Message) => void) {
+           this.page.exposeFunction(ExposedFn.OnMessage, (message: Message) =>
+             fn(message)
+           );
+         }
 
-  /**
-   * Sends a text message to given chat
-   * @param to chat id: xxxxx@us.c
-   * @param content text message
-   */
-  public async sendText(to: string, content: string) {
-    return await this.page.evaluate(
-      ({ to, content }) => {
-        WAPI.sendSeen(to);
-        WAPI.sendMessage(to, content);
-      },
-      { to, content }
-    );
-  }
+         /**
+          * Sends a text message to given chat
+          * @param to chat id: xxxxx@us.c
+          * @param content text message
+          */
+         public async sendText(to: string, content: string) {
+           return await this.page.evaluate(
+             ({ to, content }) => {
+               WAPI.sendSeen(to);
+               WAPI.sendMessage(to, content);
+             },
+             { to, content }
+           );
+         }
 
-  /**
-   * Sends a image to given chat, with caption or not, using base64
-   * @param to chat id xxxxx@us.c
-   * @param base64 base64 data:image/xxx;base64,xxx
-   * @param filename string xxxxx
-   * @param caption string xxxxx
-   */
-  public async sendImage(to: string, base64: string, filename: string, caption: string) {
-    return await this.page.evaluate(
-      ({ to, base64, filename, caption }) => {
-        WAPI.sendImage(base64, to, filename, caption);
-      },
-      { to, base64, filename, caption }
-    );
-  }
+         /**
+          * Sends a image to given chat, with caption or not, using base64
+          * @param to chat id xxxxx@us.c
+          * @param base64 base64 data:image/xxx;base64,xxx
+          * @param filename string xxxxx
+          * @param caption string xxxxx
+          */
+         public async sendImage(
+           to: string,
+           base64: string,
+           filename: string,
+           caption: string
+         ) {
+           return await this.page.evaluate(
+             ({ to, base64, filename, caption }) => {
+               WAPI.sendImage(base64, to, filename, caption);
+             },
+             { to, base64, filename, caption }
+           );
+         }
 
-  /**
-   * Sends contact card to given chat id
-   * @param {string} to 'xxxx@c.us'
-   * @param {string|array} contact 'xxxx@c.us' | ['xxxx@c.us', 'yyyy@c.us', ...]
-   */
-  public async sendContact(to: string, contactId: string | string[]) {
-    return await this.page.evaluate(
-      ({ to, contactId }) => WAPI.sendContact(to, contactId),
-      { to, contactId }
-    );
-  }
+         /**
+          * Sends contact card to given chat id
+          * @param {string} to 'xxxx@c.us'
+          * @param {string|array} contact 'xxxx@c.us' | ['xxxx@c.us', 'yyyy@c.us', ...]
+          */
+         public async sendContact(to: string, contactId: string | string[]) {
+           return await this.page.evaluate(
+             ({ to, contactId }) => WAPI.sendContact(to, contactId),
+             { to, contactId }
+           );
+         }
 
-  /**
-   * Retrieves all contacts
-   * @returns array of [Contact]
-   */
-  public async getAllContacts() {
-    return await this.page.evaluate(() => WAPI.getAllContacts());
-  }
+         /**
+          * Retrieves all contacts
+          * @returns array of [Contact]
+          */
+         public async getAllContacts() {
+           return await this.page.evaluate(() => WAPI.getAllContacts());
+         }
 
-  /**
-   * Retrieves all chats
-   * @returns array of [Chat]
-   */
-  public async getAllChats(withNewMessageOnly = false) {
-    if (withNewMessageOnly) {
-      return await this.page.evaluate(() => WAPI.getAllChatsWithNewMsg());
-    } else {
-      return await this.page.evaluate(() => WAPI.getAllChats());
-    }
-  }
+         /**
+          * Retrieves all chats
+          * @returns array of [Chat]
+          */
+         public async getAllChats(withNewMessageOnly = false) {
+           if (withNewMessageOnly) {
+             return await this.page.evaluate(() =>
+               WAPI.getAllChatsWithNewMsg()
+             );
+           } else {
+             return await this.page.evaluate(() => WAPI.getAllChats());
+           }
+         }
 
-  /**
-   * Retrieve all groups
-   * @returns array of groups
-   */
-  public async getAllGroups(withNewMessagesOnly = false) {
-    if (withNewMessagesOnly) {
-      // prettier-ignore
-      const chats = await this.page.evaluate(() => WAPI.getAllChatsWithNewMsg());
-      return chats.filter(chat => chat.isGroup);
-    } else {
-      const chats = await this.page.evaluate(() => WAPI.getAllChats());
-      return chats.filter(chat => chat.isGroup);
-    }
-  }
+         /**
+          * Retrieve all groups
+          * @returns array of groups
+          */
+         public async getAllGroups(withNewMessagesOnly = false) {
+           if (withNewMessagesOnly) {
+             // prettier-ignore
+             const chats = await this.page.evaluate(() => WAPI.getAllChatsWithNewMsg());
+             return chats.filter(chat => chat.isGroup);
+           } else {
+             const chats = await this.page.evaluate(() => WAPI.getAllChats());
+             return chats.filter(chat => chat.isGroup);
+           }
+         }
 
-  /**
-   * Retrieves group members as [Id] objects
-   * @param groupId group id
-   */
-  public async getGroupMembersId(groupId: string) {
-    return await this.page.evaluate(
-      groupId => WAPI.getGroupParticipantIDs(groupId),
-      groupId
-    );
-  }
+         /**
+          * Retrieves group members as [Id] objects
+          * @param groupId group id
+          */
+         public async getGroupMembersId(groupId: string) {
+           return await this.page.evaluate(
+             groupId => WAPI.getGroupParticipantIDs(groupId),
+             groupId
+           );
+         }
 
-  /**
-   * Returns group members [Contact] objects
-   * @param groupId
-   */
-  public async getGroupMembers(groupId: string) {
-    const membersIds = await this.getGroupMembersId(groupId);
-    const actions = membersIds.map(memberId => {
-      return this.getContact(memberId._serialized);
-    });
-    return await Promise.all(actions);
-  }
+         /**
+          * Returns group members [Contact] objects
+          * @param groupId
+          */
+         public async getGroupMembers(groupId: string) {
+           const membersIds = await this.getGroupMembersId(groupId);
+           const actions = membersIds.map(memberId => {
+             return this.getContact(memberId._serialized);
+           });
+           return await Promise.all(actions);
+         }
 
-  /**
-   * Retrieves contact detail object of given contact id
-   * @param contactId
-   * @returns contact detial as promise
-   */
-  public async getContact(contactId: string) {
-    return await this.page.evaluate(
-      contactId => WAPI.getContact(contactId),
-      contactId
-    );
-  }
-}
+         /**
+          * Retrieves contact detail object of given contact id
+          * @param contactId
+          * @returns contact detial as promise
+          */
+         public async getContact(contactId: string) {
+           return await this.page.evaluate(
+             contactId => WAPI.getContact(contactId),
+             contactId
+           );
+         }
+       }
