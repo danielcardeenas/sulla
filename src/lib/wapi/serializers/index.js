f96fb5c7c1963f4660aeb8b2d6b3c79799b57a5c
danielcardeenas <danielcardeenas@gmail.com>
2020-01-25 11:14:36 -0600

    Separate serializers

--- src/lib/wapi/serializers/index.js
+++ src/lib/wapi/serializers/index.js
@@ -3,3 +3,4 @@ export { _serializeRawObj } from './serialize-raw';
 export { _serializeMessageObj } from './serialize-message';
 export { _serializeContactObj } from './serialize-contact';
 export { _serializeNumberStatusObj } from './serialize-number-status';
+export { _serializeProfilePicThumb } from './serialize-profile-pic-thumb';
