46d3ca8ba0b04a1cba2f88ba6224789994497fba
Restyled.io <commits@restyled.io>
2020-04-09 13:49:59 +0000

    Restyled by prettier

--- test/sendFile.js
+++ test/sendFile.js
@@ -1,21 +1,25 @@
-const {create} = require('../dist/index');
+const { create } = require('../dist/index');
 
 var client;
 
 async function startClient() {
   client = await create('session', () => console.log('QR Code received'), {
-    headless : false,
-    devtools : true,
+    headless: false,
+    devtools: true
   });
 
-  client.onStateChange((state) => {
+  client.onStateChange(state => {
     if (state === 'UNLAUNCHED') {
       client.useHere();
     }
   });
 
-  await client.sendFile(process.argv[2] + '@c.us', './media/GoPro.mp4',
-                        'GoPro.mp4', 'Test file send');
+  await client.sendFile(
+    process.argv[2] + '@c.us',
+    './media/GoPro.mp4',
+    'GoPro.mp4',
+    'Test file send'
+  );
 }
 
 console.log('Test send file to: ' + process.argv[2]);
