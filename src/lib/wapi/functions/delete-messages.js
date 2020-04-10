220bf6779ebee45f9b7c8f22065e2fdab233f89b
Restyled.io <commits@restyled.io>
2020-04-10 14:52:48 +0000

    Restyled by clang-format

--- src/lib/wapi/functions/delete-messages.js
+++ src/lib/wapi/functions/delete-messages.js
@@ -1,8 +1,10 @@
 /**
  * Deletes message of given message id
  * @param {string} chatId The chat id from which to delete the message.
- * @param {string[]} messageArray The specific message id of the message to be deleted
- * @param {boolean} onlyLocal If it should only delete locally (message remains on the other recipienct's phone). Defaults to false.
+ * @param {string[]} messageArray The specific message id of the message to be
+ *     deleted
+ * @param {boolean} onlyLocal If it should only delete locally (message remains
+ *     on the other recipienct's phone). Defaults to false.
  * @param {Function} done Optional callback
  */
 export function deleteMessages(chatId, messageArray, onlyLocal, done) {
@@ -16,27 +18,27 @@ export function deleteMessages(chatId, messageArray, onlyLocal, done) {
   }
 
   if (!Array.isArray(messageArray)) {
-    messageArray = [messageArray];
+    messageArray = [ messageArray ];
   }
 
-  let messagesToDelete = messageArray
-    .map((msgId) =>
-      typeof msgId == 'string' ? window.Store.Msg.get(msgId) : msgId
-    )
-    .filter((x) => x);
-  if (messagesToDelete.length == 0) return true;
-  let jobs = onlyLocal
-    ? [conversation.sendDeleteMsgs(messagesToDelete, conversation)]
-    : [
-        conversation.sendRevokeMsgs(
-          messagesToDelete.filter((msg) => msg.isSentByMe),
-          conversation
-        ),
-        conversation.sendDeleteMsgs(
-          messagesToDelete.filter((msg) => !msg.isSentByMe),
-          conversation
-        ),
-      ];
+  let messagesToDelete =
+      messageArray
+          .map((msgId) => typeof msgId == 'string' ? window.Store.Msg.get(msgId)
+                                                   : msgId)
+          .filter((x) => x);
+  if (messagesToDelete.length == 0)
+    return true;
+  let jobs =
+      onlyLocal
+          ? [ conversation.sendDeleteMsgs(messagesToDelete, conversation) ]
+          : [
+              conversation.sendRevokeMsgs(
+                  messagesToDelete.filter((msg) => msg.isSentByMe),
+                  conversation),
+              conversation.sendDeleteMsgs(
+                  messagesToDelete.filter((msg) => !msg.isSentByMe),
+                  conversation),
+            ];
   return Promise.all(jobs).then((_) => {
     if (done !== undefined) {
       done(true);
