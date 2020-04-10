af14a3ed4471255796ed198c2a4779454b9dd659
Restyled.io <commits@restyled.io>
2020-04-10 14:52:50 +0000

    Restyled by prettier

--- src/lib/wapi/functions/delete-messages.js
+++ src/lib/wapi/functions/delete-messages.js
@@ -18,28 +18,28 @@ export function deleteMessages(chatId, messageArray, onlyLocal, done) {
   }
 
   if (!Array.isArray(messageArray)) {
-    messageArray = [ messageArray ];
+    messageArray = [messageArray];
   }
 
-  let messagesToDelete =
-      messageArray
-          .map((msgId) => typeof msgId == 'string' ? window.Store.Msg.get(msgId)
-                                                   : msgId)
-          .filter((x) => x);
-  if (messagesToDelete.length == 0)
-    return true;
-  let jobs =
-      onlyLocal
-          ? [ conversation.sendDeleteMsgs(messagesToDelete, conversation) ]
-          : [
-              conversation.sendRevokeMsgs(
-                  messagesToDelete.filter((msg) => msg.isSentByMe),
-                  conversation),
-              conversation.sendDeleteMsgs(
-                  messagesToDelete.filter((msg) => !msg.isSentByMe),
-                  conversation),
-            ];
-  return Promise.all(jobs).then((_) => {
+  let messagesToDelete = messageArray
+    .map(msgId =>
+      typeof msgId == 'string' ? window.Store.Msg.get(msgId) : msgId
+    )
+    .filter(x => x);
+  if (messagesToDelete.length == 0) return true;
+  let jobs = onlyLocal
+    ? [conversation.sendDeleteMsgs(messagesToDelete, conversation)]
+    : [
+        conversation.sendRevokeMsgs(
+          messagesToDelete.filter(msg => msg.isSentByMe),
+          conversation
+        ),
+        conversation.sendDeleteMsgs(
+          messagesToDelete.filter(msg => !msg.isSentByMe),
+          conversation
+        )
+      ];
+  return Promise.all(jobs).then(_ => {
     if (done !== undefined) {
       done(true);
     }
